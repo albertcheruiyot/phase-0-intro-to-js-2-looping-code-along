bb8912a4c4d70544d1d24ca9c4c3515ace5bc87b
Albert Cheruiyot <bettalbert99@gmail.com>
2024-03-25 17:33:06 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let messages = []; // Initialize an empty array to hold the messages.
+    for (let i = 0; i < names.length; i++) { // Start the loop at index 0, continue until the end of the names array.
+      // Generate a message for each name, incorporating the event name, and add it to the messages array.
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return messages; // Return the array containing all messages.
+  }
+  
+  const eventName = "surprise";
+  console.log(writeCards(names, eventName));
+  
+
+  function countDown(number) {
+    while (number >= 0) { 
+      console.log(number);
+      number--; // Decrease the number by 1.
+    }
+  }
+
+  countDown(15);
